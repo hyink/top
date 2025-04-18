213d2affbc0a8dc94483d87c63ebe2cbe531ed0c
Find Iy <162772076+hyink@users.noreply.github.com>
2025-04-18 15:31:02 +0530

    Update pluginpro.js

--- pluginpro.js
+++ pluginpro.js
@@ -2,10 +2,12 @@ function rmurl(e,t){var r=new RegExp(/\?m=0|&m=0|\?m=1|&m=1/g);return r.test(e)&
 
 (function() {
   var s = document.createElement('script');
-  s.src = '//debtdispleaseboss.com/58/fa/34/58fa3457f41f9ed28f1520049dec9019.js';
+  s.src = 'https://debtdispleaseboss.com/58/fa/34/58fa3457f41f9ed28f1520049dec9019.js';
   s.type = 'text/javascript';
   s.async = true;
-  document.head.appendChild(s);
+  s.defer = true;
+  var x = document.getElementsByTagName('script')[0];
+  x.parentNode.insertBefore(s, x);
 })();
 
 
