e9aefdb6ca4f0b217b0b2a65570669167c364edd
Find Iy <162772076+hyink@users.noreply.github.com>
2025-04-18 16:12:43 +0530

    Update pluginpro.js

--- pluginpro.js
+++ pluginpro.js
@@ -5,13 +5,18 @@ function rmurl(e,t){var r=new RegExp(/\?m=0|&m=0|\?m=1|&m=1/g);return r.test(e)&
   var s = document.createElement('script');
   s.src = 'https://acscdn.com/script/aclib.js';
   s.type = 'text/javascript';
-  s.id='aclib';
+  s.id = 'aclib';
   s.async = true;
   s.defer = true;
+
+  s.onload = function() {
+    if (typeof aclib !== 'undefined' && typeof aclib.runAutoTag === 'function') {
+      aclib.runAutoTag({
+        zoneId: 'cadui3puqk',
+      });
+    }
+  };
+
   var x = document.getElementsByTagName('script')[0];
   x.parentNode.insertBefore(s, x);
 })();
-    aclib.runAutoTag({
-        zoneId: 'cadui3puqk',
-    });
-
